2c0fd725ae2065d41c824a71742e71ea44b9eb1f
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2025-04-27 00:05:32 +0300

    js task2

---
+++ script.js
@@ -0,0 +1,53 @@
+
+function addTask() {
+    const taskText = document.getElementById('taskInput').value.trim();
+    if (taskText === '') return;
+
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.onclick = function () {
+        li.classList.toggle('completed');
+    };
+    checkbox.className = 'check';
+
+    const span = document.createElement('span');
+    span.className = 'text';
+    span.textContent = taskText;
+
+    span.onclick = function () {
+        const currentText = span.textContent;
+        const inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.value = currentText;
+        span.innerHTML = '';
+        span.appendChild(inputField);
+        inputField.focus();
+
+        inputField.onblur = function () {
+            const updatedText = inputField.value.trim();
+            if (updatedText !== '')
+                span.textContent = updatedText;
+            else 
+                span.textContent = currentText;
+        };
+        inputField.onkeydown = function (event) {
+            if (event.key === 'Enter') {
+                inputField.onblur();
+            }
+        };
+    };
+
+    const delBtn = document.createElement('button');
+    delBtn.innerHTML = 'x';
+    delBtn.className = 'delete-btn';
+    delBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    document.getElementById('taskList').appendChild(li);
+    document.getElementById('taskInput').value = '';
+}
